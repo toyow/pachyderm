2a6ad7575077c3a647ebeeeb627a251467ba76c8
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-04-13 14:43:39 -0400

    rename postgres test container

--- etc/testing/start_postgres.sh
+++ etc/testing/start_postgres.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-if ! docker ps | grep -q postgres
+if ! docker ps | grep -q postgres_test
 then
     echo "starting postgres..."
-    docker run -d \
+    docker run -d --rm \
+    --name postgres_test \
     -e POSTGRES_HOST_AUTH_METHOD=trust \
     -p 5432:5432 \
     postgres:13.0-alpine
